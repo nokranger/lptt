4769f8b6fae22617236939f7be3209a9cc904f09
nokranger <anusorn.thavornporn@gmail.com>
2019-09-26 18:05:54 +0700

    ref. fix bug

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,12 @@ module.exports = {
   extends: ['plugin:vue/essential', '@vue/standard'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'object-curly-spacing': ['error', 'always'],
+    'space-before-function-paren': [
+      'error',
+      {anonymous: 'always', named: 'always', asyncArrow: 'always'}
+    ]
   },
   parserOptions: {
     parser: 'babel-eslint'
--- src/views/signup.vue
+++ src/views/signup.vue
@@ -1,68 +1,100 @@
 <template>
   <div>
-    <meta charset="utf-8">
-    <meta content="width=device-width,initial-scale=1,minimal-ui" name="viewport">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" integrity="sha384-3AB7yXWz4OeoZcPbieVW64vVXEwADiYyAEhwilzWsLw+9FgqpyjjStpPnpBO8o8S" crossorigin="anonymous">
+    <meta charset="utf-8" />
+    <meta content="width=device-width,initial-scale=1,minimal-ui" name="viewport" />
+    <link
+      rel="stylesheet"
+      href="https://use.fontawesome.com/releases/v5.0.8/css/all.css"
+      integrity="sha384-3AB7yXWz4OeoZcPbieVW64vVXEwADiYyAEhwilzWsLw+9FgqpyjjStpPnpBO8o8S"
+      crossorigin="anonymous"
+    />
     <b-container>
       <b-row>
         <b-col cols="12" sm="12" md="12" lg="3" xl="3"></b-col>
         <b-col cols="12" sm="12" md="12" lg="6" xl="6">
-          <br>
+          <br />
           <div>
-            <strong style="text-align:center;font-weight:bolder;color: #4f4f4f!important;">Create your personal account</strong>
+            <strong style="text-align:center;font-weight:bolder;color: #4f4f4f!important;">
+              Create your personal
+              account
+            </strong>
           </div>
-          <br>
+          <br />
           <b-card>
-          <b-form @submit="onSubmit" v-if="show">
-            <div>
-              <b-form-group id="input-group-1">
-                <label class="forminput">Username <label style="color:red;">*</label></label>
-                <b-form-input
-                  id="input-1"
-                  v-model="form.username"
-                  type="text"
-                  required
-                  placeholder=""
-                ></b-form-input>
-                <label style="font-size:10px;display:block;text-align:left;color:gray;">We'll never share your email with anyone else.</label>
-              </b-form-group>
-            </div>
-            <div>
-              <b-form-group id="input-group-3">
-                <label class="forminput">Email address <label style="color:red;">*</label></label>
-                <b-form-input
-                  id="input-3"
-                  v-model="form.email"
-                  type="email"
-                  required
-                  placeholder=""
-                ></b-form-input>
-                <label style="font-size:10px;display:block;text-align:left;color:gray;">We’ll occasionally send updates about your account to this inbox. We’ll never share your email address with anyone.</label>
-              </b-form-group>
-            </div>
-
-            <b-form-group id="input-group-2">
-              <label class="forminput">Password <label style="color:red;">*</label></label>
-              <b-form-input
-                id="input-2"
-                v-model="form.password"
-                type="password"
-                required
-                placeholder=""
-              ></b-form-input>
-              <label style="font-size:10px;display:block;text-align:left;color:gray;">Make sure it's at least 15 characters OR at least 8 characters including a number and a lowercase letter. Learn more.</label>
-            </b-form-group>
-            <div>
-            <b-button class="blue-gradient btn-block" type="submit">Create an account</b-button><br>
-            <label>or Sign up with</label>
-            </div>
-            <div>
+            <b-form @submit="onSubmit" v-if="show">
               <div>
-                <b-button class="socialb" style="margin-right:5px;" @click="socialGoogleLogin"><i class="fab fa-google"></i></b-button>
-                <b-button class="socialb"><i class="fab fa-facebook" @click="socialFacecbookLogin"></i></b-button>
+                <b-form-group id="input-group-1">
+                  <label class="forminput">
+                    Username
+                    <label style="color:red;">*</label>
+                  </label>
+                  <b-form-input id="input-1" v-model="form.username" type="text" required placeholder></b-form-input>
+                  <label style="font-size:10px;display:block;text-align:left;color:gray;">
+                    We'll never share your email
+                    with anyone else.
+                  </label>
+                </b-form-group>
               </div>
-            </div>
-          </b-form>
+              <div>
+                <b-form-group id="input-group-3">
+                  <label class="forminput">
+                    Email address
+                    <label style="color:red;">*</label>
+                  </label>
+                  <b-form-input id="input-3" v-model="form.email" type="email" required placeholder></b-form-input>
+                  <label style="font-size:10px;display:block;text-align:left;color:gray;">
+                    We’ll occasionally send
+                    updates about your account to this inbox. We’ll never share your email address with anyone.
+                  </label>
+                </b-form-group>
+              </div>
+              <div>
+                <b-form-group id="input-group-2">
+                  <label class="forminput">
+                    Password
+                    <label style="color:red;">*</label>
+                  </label>
+                  <b-form-input id="input-2" v-model="form.password" type="password" required placeholder></b-form-input>
+                  <label style="font-size:10px;display:block;text-align:left;color:gray;">
+                    Make sure it's at least 15
+                    characters OR at least 8 characters including a number and a lowercase letter. Learn more.
+                  </label>
+                </b-form-group>
+              </div>
+              <div>
+                <b-form-group id="input-group-1">
+                  <label class="forminput">
+                    Name
+                    <label style="color:red;">*</label>
+                  </label>
+                  <b-form-input id="input-4" v-model="form.name" type="text" required placeholder></b-form-input>
+                </b-form-group>
+              </div>
+              <div>
+                <div>
+                  <b-form-group id="input-group-1">
+                    <label class="forminput">
+                      Lastname
+                      <label style="color:red;">*</label>
+                    </label>
+                    <b-form-input id="input-4" v-model="form.name" type="text" required placeholder></b-form-input>
+                  </b-form-group>
+                </div>
+                <b-button class="blue-gradient btn-block" type="submit">Create an account</b-button>
+                <br />
+                <label>or Sign up with</label>
+              </div>
+              <div>
+                <div>
+                  <b-button class="socialb" style="margin-right:5px;" @click="socialGoogleLogin">
+                    <i class="fab fa-google"></i>
+                  </b-button>
+                  <b-button class="socialb">
+                    <i class="fab fa-facebook" @click="socialFacecbookLogin"></i>
+                  </b-button>
+                </div>
+              </div>
+            </b-form>
           </b-card>
         </b-col>
         <b-col cols="12" sm="12" md="12" lg="3" xl="3"></b-col>
@@ -82,7 +114,7 @@ export default {
       amp: true
     }
   },
-  data () {
+  data() {
     return {
       form: {
         username: '',
@@ -93,11 +125,11 @@ export default {
     }
   },
   methods: {
-    onSubmit (evt) {
+    onSubmit(evt) {
       evt.preventDefault()
       alert(JSON.stringify(this.form))
     },
-    onReset (evt) {
+    onReset(evt) {
       evt.preventDefault()
       // Reset our form values
       this.form.email = ''
@@ -109,7 +141,7 @@ export default {
         this.show = true
       })
     },
-    socialGoogleLogin: function () {
+    socialGoogleLogin() {
       const provide = new firebase.auth.GoogleAuthProvider().addScope('email')
       firebase
         .auth()
@@ -129,7 +161,7 @@ export default {
           alert('Oops. ' + err.message)
         })
     },
-    socialFacecbookLogin: function () {
+    socialFacecbookLogin() {
       const provide = new firebase.auth.FacebookAuthProvider()
       firebase
         .auth()
